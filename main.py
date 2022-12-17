e2449368e17cee8df6fc9ca52dd00f417ffa6c2b
Kai Piper <kai9987kai@gmail.com>
2022-12-17 19:38:15 +0000

    Create main.py

---
+++ main.py
@@ -0,0 +1,59 @@
+import tkinter as tk
+from tkinter import filedialog
+from PIL import Image
+
+# Create the main window
+window = tk.Tk()
+window.title("3D Texture Map Converter")
+
+# Function to open an image file and display it in the label
+def open_image():
+  # Open a file dialog to select an image file
+  filepath = filedialog.askopenfilename()
+  # Load the image from the file
+  image = Image.open(filepath)
+  # Resize the image to fit in the label
+  image = image.resize((300, 300), Image.ANTIALIAS)
+  # Update the label with the new image
+  image_label.config(image=image)
+  image_label.image = image
+
+# Function to convert the image to a normal map
+def convert_to_normal_map():
+  # Open the image file
+  image = Image.open(filepath)
+  # Convert the image to a normal map using some algorithm
+  new_image = normal_map_conversion(image)
+  # Update the label with the new image
+  image_label.config(image=new_image)
+  image_label.image = new_image
+
+# Function to convert the image to a specular map
+def convert_to_specular_map():
+  # Open the image file
+  image = Image.open(filepath)
+  # Convert the image to a specular map using some algorithm
+  new_image = specular_map_conversion(image)
+  # Update the label with the new image
+  image_label.config(image=new_image)
+  image_label.image = new_image
+
+# Add a button to open an image file
+open_button = tk.Button(text="Open Image", command=open_image)
+open_button.pack()
+
+# Add a button to convert the image to a normal map
+normal_button = tk.Button(text="Convert to Normal Map", command=convert_to_normal_map)
+normal_button.pack()
+
+# Add a button to convert the image to a specular map
+specular_button = tk.Button(text="Convert to Specular Map", command=convert_to_specular_map)
+specular_button.pack()
+
+# Add a label to display the image
+image_label = tk.Label(image=None)
+image_label.pack()
+
+# Run the main loop
+window.mainloop()
+
