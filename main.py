84a12162061c2132cfe1194f96d617687458f84b
Kai Piper <kai9987kai@gmail.com>
2022-12-19 00:43:24 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-import tkinter as tk
+import tkinter
 from tkinter import filedialog
+from tkinter import PhotoImage
 from PIL import Image
+from PIL import ImageTk
 
 # Create the main window
-window = tk.Tk()
+window = tkinter.Tk()
 window.title("3D Texture Map Converter")
 
 # Function to open an image file and display it in the label
@@ -14,48 +16,55 @@ def open_image():
   image = Image.open(filepath)
   # Resize the image to fit in the label
   image = image.resize((300, 300), Image.ANTIALIAS)
+  # Convert the image to a PhotoImage object
+  photo_image = ImageTk.PhotoImage(image=image)
   # Update the label with the new image
-  image_label.config(image=image)
-  image_label.image = image
+  image_label.config(image=photo_image)
+  image_label.image = photo_image
+  # Call the conversion functions with the filepath as an argument
+  convert_to_normal_map(filepath)
+  convert_to_specular_map(filepath)
 
 # Function to convert the image to a normal map
-def convert_to_normal_map():
+def convert_to_normal_map(filepath):
   # Open the image file
   image = Image.open(filepath)
   # Convert the image to a normal map using some algorithm
   new_image = normal_map_conversion(image)
+  # Convert the new image to a PhotoImage object
+  photo_image = ImageTk.PhotoImage(image=new_image)
   # Update the label with the new image
-  image_label.config(image=new_image)
-  image_label.image = new_image
+  image_label.config(image=photo_image)
+  image_label.image = photo_image
 
 # Function to convert the image to a specular map
-def convert_to_specular_map():
+def convert_to_specular_map(filepath):
   # Open the image file
   image = Image.open(filepath)
   # Convert the image to a specular map using some algorithm
   new_image = specular_map_conversion(image)
+  # Convert the new image to a PhotoImage object
+  photo_image = ImageTk.PhotoImage(image=new_image)
   # Update the label with the new image
-  image_label.config(image=new_image)
-  image_label.image = new_image
+  image_label.config(image=photo_image)
+  image_label.image = photo_image
 
 # Add a button to open an image file
-open_button = tk.Button(text="Open Image", command=open_image)
+open_button = tkinter.Button(text="Open Image", command=open_image)
 open_button.pack()
 
 # Add a button to convert the image to a normal map
-normal_button = tk.Button(text="Convert to Normal Map", command=convert_to_normal_map)
+normal_button = tkinter.Button(text="Convert to Normal Map", command=convert_to_normal_map)
 normal_button.pack()
 
 # Add a button to convert the image to a specular map
-specular_button = tk.Button(text="Convert to Specular Map", command=convert_to_specular_map)
+specular_button = tkinter.Button(text="Convert to Specular Map", command=convert_to_specular_map)
 specular_button.pack()
 
 # Add a label to display the image
-image_label = tk.Label(image=None)
+image_label = tkinter.Label(image=None)
 image_label.pack()
 
 # Run the main loop
 window.resizable(False, False)
 window.mainloop()
-
-
